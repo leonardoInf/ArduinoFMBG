73c653ed476787e4e9747d6ba74c2baba7280267
Leonardo <mondloslp@gmail.com>
2019-06-19 21:25:33 +0200

    Kompabilität mit UNO

--- ArduinoPhysik/setup.h
+++ ArduinoPhysik/setup.h
@@ -1,6 +1,9 @@
 //#if defined(WLAN) || defined(SERIELLE_AUSGABE) 
   Serial.begin(9600);
-  DIDR0 = 0; DIDR2 = 0;       //Analoge Pins konfigurieren
+  DIDR0 = 0;      //Analoge Pins konfigurieren
+  #ifndef UNO
+  DIDR2 = 0; 
+  #endif
   //#endif
   #ifdef SERIELLE_AUSGABE
   Serial.println("zSerial alive");
